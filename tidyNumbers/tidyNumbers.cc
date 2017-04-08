41f39cc67ce88586ecbb950ac4aaaf1e676013ea
Dirken <ricard.meyerhofer@est.fib.upc.edu>
2017-04-09 01:24:42 +0200

    tidy had a stupid problem

--- tidyNumbers/tidyNumbers.cc
+++ tidyNumbers/tidyNumbers.cc
@@ -3,24 +3,6 @@
 #include <algorithm>
 using namespace std;  
 
-long long next_tidy_number(long long number){
-	vector<int> v;
-	while (number >= 10){
-		v.insert(v.begin(), number%10);
-		number = number/10;
-	}
-	for (int i = 1; i <= v.size(); ++i){
-		if (v[i-1] > v[i]) {
-			for (int j = i; j <= v.size(); ++j)v[j] = 0;
-		}
-	}
-	long long aux = 0;
-	for (long long i = 0; i < v.size(); ++i){
-		aux = aux * 10 + v[i];
-	}
-	return aux-1;
-}
-
 bool is_tidy_number(long long number){
 	while (number >= 10){
 		int aux1 = number%10;
@@ -30,13 +12,37 @@ bool is_tidy_number(long long number){
 	}
 	return true;
 }
+
+long long next_tidy_number(long long number){
+	vector<int> v;
+	if (not is_tidy_number(number)){
+		while (number >= 10){
+			v.insert(v.begin(), number%10);
+			number = number/10;
+		}
+		v.insert(v.begin(), number);
+		long long aux = 0;
+		for (long long i = 1; i <= v.size(); ++i){
+			if (v[i-1] > v[i]) {
+				for (long long j = i; j <= v.size(); ++j) v[j] = 0;
+				
+			}	
+		}	
+		for (long long k = 0; k < v.size(); ++k){
+			aux = aux * 10 + v[k];
+		}
+		return aux;
+	}
+	return number;
+}
+
+
 long long tidy_number(long long number){
-	number = next_tidy_number(number);
 	while (number >= 0){
 		if (is_tidy_number(number)) return number;
-		number = next_tidy_number(number);
-		cout << number << endl;
+		number = next_tidy_number(number-1);
 	}
+	return number;
 }
 
 
