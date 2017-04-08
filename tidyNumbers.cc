218346564ee210810b7477ff3700746d28d3a5ef
Dirken <ricard.meyerhofer@est.fib.upc.edu>
2017-04-08 19:54:33 +0200

    tidy numbers finished

--- tidyNumbers.cc
+++ tidyNumbers.cc
@@ -1,28 +1,33 @@
-#include <iostream>  // includes cin to read from stdin and cout to write to stdout
-using namespace std;  // since cin and cout are both in namespace std, this saves some text
+#include <iostream> 
+#include <vector>
+#include <algorithm>
+using namespace std;  
 
-
-int tidy_number(int number){
-	while (number >= 0){
-		if (is_tidy_number(number)) return number;
-		--number;
-	}
-}
-
-bool is_tidy_number(int number){
+bool is_tidy_number(long long number){
 	while (number >= 10){
-		int aux1 = number%10:
+		int aux1 = number%10;
 		number = number/10;
 		int aux2 = number%10;
-		if (aux2 < aux1) return false;
+		if (aux2 > aux1) return false;
 	}
-	return true;
 }
 
+long long tidy_number(long long number){
+	while (number >= 0){
+		if (is_tidy_number(number)) return number;
+		else{
+			if (number%100 == 10) number = number-number/10;
+			else number = number - number%10 - 1;
+		}
+
+	}
+}
+
+
 int main() {
-  int t, n;
+  long long t, n;
   cin >> t; 
-  for (int i = 1; i <= t; ++i) {
+  for (long long i = 1; i <= t; ++i) {
     cin >> n; 
     cout << "Case #" << i << ": " << tidy_number(n) << endl;
   }
