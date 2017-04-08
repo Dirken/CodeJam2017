8c09308e302e3b63840e9c87a19715272f63bf90
Dirken <ricard.meyerhofer@est.fib.upc.edu>
2017-04-09 00:26:08 +0200

    now i will update on the platform

--- tidyNumbers/tidyNumbers.cc
+++ tidyNumbers/tidyNumbers.cc
@@ -3,6 +3,24 @@
 #include <algorithm>
 using namespace std;  
 
+long long next_tidy_number(long long number){
+	vector<int> v;
+	while (number >= 10){
+		v.insert(v.begin(), number%10);
+		number = number/10;
+	}
+	for (int i = 1; i <= v.size(); ++i){
+		if (v[i-1] > v[i]) {
+			for (int j = i; j <= v.size(); ++j)v[j] = 0;
+		}
+	}
+	long long aux = 0;
+	for (long long i = 0; i < v.size(); ++i){
+		aux = aux * 10 + v[i];
+	}
+	return aux-1;
+}
+
 bool is_tidy_number(long long number){
 	while (number >= 10){
 		int aux1 = number%10;
@@ -10,18 +28,17 @@ bool is_tidy_number(long long number){
 		int aux2 = number%10;
 		if (aux2 > aux1) return false;
 	}
+	return true;
 }
-
 long long tidy_number(long long number){
+	number = next_tidy_number(number);
 	while (number >= 0){
 		if (is_tidy_number(number)) return number;
-		else{
-			if (number%100 == 10) number = number-number/10;
-			else number = number - number%10 - 1;
-		}
-
+		number = next_tidy_number(number);
+		cout << number << endl;
 	}
 }
+
 
 
 int main() {
