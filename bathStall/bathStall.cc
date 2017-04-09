fcb570d1268cafe472453e18e22257eb0e24f744
Dirken <ricard.meyerhofer@est.fib.upc.edu>
2017-04-09 02:54:36 +0200

    bathstall correct

--- bathStall/bathStall.cc
+++ bathStall/bathStall.cc
@@ -1,35 +1,35 @@
 #include <iostream> 
 #include <vector>
+#include <cmath>
 #include <algorithm>
 using namespace std;  
 
 
-long long stallDistance(long long stalls, long long people){
-	if (people == 1) return stalls -1;
-  else{
-    if (people%2 == 0) return stallDistance(stalls/2, people/2);
-    else{
-        if (stalls%2 == 0) return stallDistance(stalls/2 -1, people/2);
-        else return stallDistance(stalls/2, people/2);
-    }
-  }
+long long bathProblem(long long N, long long K){
+  int level = floor(log(K)/log(2));
+  long long people = pow(2,level);
+  long long seats = N - people + 1;
+  long long seatsCeil = ceil(1.0*seats/people);
+  long long seatsFloor = floor(1.0*seats/people);
+  seats = seats - seatsFloor*people;
+  if(K - pow(2,level) + 1 <= seats) return seatsCeil;
+  return seatsFloor;
 }
-
 int main() {
   long long t, stalls, people, r, min, max;
   cin >> t; 
   for (int i = 1; i <= t; ++i) {
     cin >> stalls >> people;
-    r = stallDistance(stalls,people);
-    if (r%2 == 0) {
-      min = max = r/2;
+    long long effectiveSize = bathProblem(stalls,people);
+    cout << "Case #" << i << ": ";
+    if(effectiveSize <= 1){
+      cout << 0 << " " << 0 << endl;
+    }else{
+      long long upper = ceil(1.0*(effectiveSize-1)/2);
+      long long lower = floor(1.0*(effectiveSize-1)/2);
+      cout << upper << ' ' << lower << endl;
     }
-    else {
-      max = (r+1)/2;
-      min = (r-1)/2;
-    }
-    cout << "Case #" << i << ": " << max << ' ' << min << endl;
-  }
+}
 }
 
 
